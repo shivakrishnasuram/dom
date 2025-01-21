e3d8821a9b95f6a88ccbe733afd39a4784933722
shivakrishnasuram <suramshivakrishnasuram@gmail.com>
2025-01-21 18:16:04 +0530

    initials

---
+++ arraymethods.js
@@ -0,0 +1,47 @@
+// let arr =[1,2,4,5,1,2]
+// let result =arr.toString();
+// console.log(typeof(result))
+// console.log(typeof(arr))
+// console.log(result)
+
+//join 
+// let one =[2,2,3,4,2,9]
+// let two =[2,24,2,9]
+// let op =one.join("-")
+// console.log(op)
+
+
+// let arr2 = [1,2,3,4,5,6,7,8,9,10]
+// let op =arr2.slice(2,5)
+// console.log(op)
+// it gives the output from 2 to 5 index
+// [3,4,5]
+// let op1 =arr2.slice(2)
+// console.log(op1)
+// it gives the output from 2 to end of the array
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// arr.shift()
+// console.log(arr)
+
+// it will remove the first element of the array
+// console.log(arr.length)//10
+// arr.unshift(0)// it will add the element at the first index
+// console.log(arr.length)//11
+// console.log(arr)
+
+//  let arr = [1,2,3,4,5,6,7,"sjobs",8,9,10]
+//  let mutton =arr.slice(2,7)
+//  console.log(mutton)
+
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let mutton =arr.splice(2,7)
+// console.log(mutton)
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10] 
+let mutton =arr.splice(2,7,100,200,300)
+console.log(mutton)
+
+
